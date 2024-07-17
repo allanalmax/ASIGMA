044bb9dca58cffd3f5e285bf13c5d58e78ed6c57
Amanya Allan Kayumbu <allanalmax13@gmail.com>
2024-07-17 14:20:21 +0300

    added new changes

--- flutter/tutorial.dart
+++ flutter/tutorial.dart
@@ -1,11 +1,30 @@
 void main() {
-  int x = 20;
-  int sign;
-
-  if (x > 0) {
-    sign = 1;
-  } else {
-    sign = -1;
+  var restaurants = [
+    {
+      'name' : 'Pizza Mario',
+      'cuisine' : 'Italian',
+      'ratings' : [5.0, 3.5, 4.5],
+    },
+    {
+      'name' : 'Chez Anne',
+      'cuisine' : 'Indian',
+      'ratings' : [5.0, 4.5, 4.0],
+    },
+    {
+      'name' : 'Navaratna',
+      'cuisine' : 'Italian',
+      'ratings' : [4.0, 4.5, 4.0],
+    },
+  ];
+  
+  for (var restaurant in restaurants) {
+    final ratings = restaurant['ratings'] as List<double>;
+    var total = 0.0;
+    for (var rating in ratings) {
+      total += rating;
+    }
+    final avgRating = total / ratings.length;
+    restaurant['avgrating'] = avgRating;
+    print(restaurant);
   }
-  print(sign);
 }
