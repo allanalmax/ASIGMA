061a7a87d2b11c63a0562def0658c1c88f180dfa
AzureAD\AllanAmanya <allan.amanya@asigmagroup.com>
2024-07-17 14:27:16 +0300

    solving conflicts

--- C#/HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ C#/HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6e8219f214ba2b4394f164d161ef1fca51853c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9e8bf5f9e0a17017b5da83daa99ae86c2536017")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- flutter_tutorials/rock_paper_scissors/rock_paper_scissors.dart
+++ flutter_tutorials/rock_paper_scissors/rock_paper_scissors.dart
@@ -1,4 +1,4 @@
-import 'dart.io';
+import 'dart:io';
 
 void main () {
   print('Rock, Paper, Scissors? (r/p/s)');
