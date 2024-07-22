cb3dc7feb69aefce08425423ddf341b59b41fb9d
AzureAD\AllanAmanya <allan.amanya@asigmagroup.com>
2024-07-22 18:41:31 +0300

    new changes

--- C#/HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ C#/HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9e8bf5f9e0a17017b5da83daa99ae86c2536017")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e598bb845746b13f507e3049b8317d73668d64c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
