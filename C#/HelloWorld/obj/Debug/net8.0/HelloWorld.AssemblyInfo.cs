648e86540cc945eaf99e81b9c498bdf1d489d027
Amanya Allan Kayumbu <allanalmax13@gmail.com>
2024-07-28 19:26:46 +0300

    new C# changes

--- C#/HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ C#/HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fab4fcf8b75dc9aa7b9c8c612421342d70eaadf2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a22119aeb38b3165afdd54312ad0230818f26a79")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
