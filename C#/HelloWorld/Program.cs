71aab2377c022dfa7a31d47ad1def305a8defce6
AzureAD\AllanAmanya <allan.amanya@asigmagroup.com>
2024-08-09 18:05:43 +0300

    continuation of C# tutorial

--- C#/HelloWorld/Program.cs
+++ C#/HelloWorld/Program.cs
@@ -1,4 +1,4 @@
-﻿namespace HelloWorld;
+﻿/* namespace HelloWorld;
 
 class Program
 {
@@ -10,5 +10,56 @@ class Program
         Console.Write($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!");
         Console.Write($"{Environment.NewLine}Press any key to exit...");
         Console.ReadKey(true);
+    }
+} */
+
+/*STRINGS
+ * 
+ * Console.WriteLine("Hello David!");
+
+string firstFriend = "    Maria    ";
+firstFriend = firstFriend.TrimStart();
+
+string secondFriend = "    Scott    ";
+
+string friends = $"My friends are {firstFriend} and {secondFriend.Trim()}.";
+Console.WriteLine(friends);
+Console.WriteLine(friends.Contains("Scott"));
+Console.WriteLine(friends.ToUpper());
+Console.WriteLine(friends.Length);
+Console.WriteLine(friends.StartsWith("My  "));*/
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        /*//Strings
+        Console.WriteLine("Hello David!");
+
+        string firstFriend = "    Maria    ";
+        firstFriend = firstFriend.TrimStart();
+
+        string secondFriend = "    Scott    ";
+
+        string friends = $"My friends are {firstFriend} and {secondFriend.Trim()}.";
+        Console.WriteLine(friends);
+        Console.WriteLine(friends.Contains("Scott"));
+        Console.WriteLine(friends.ToUpper());
+        Console.WriteLine(friends.Length);
+        Console.WriteLine(friends.StartsWith("My  "));*/
+
+        //Numbers
+        int a = 18;
+        int b = 6;
+        int c = a + b;
+        Console.WriteLine(c);
+
+        /*double d = 18.3; // natural type*/
+        decimal d = 32.2M; // explicit type
+        decimal e = 6.32M;
+        decimal f = checked(d + e);
+        Console.WriteLine($"The answer is {f}");
     }
 }
